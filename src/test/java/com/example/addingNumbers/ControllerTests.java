b704ec6e4b5d8d19383ce00cd4d53a219b4e5c06
saiswethadonthineni <donthinenisaiswetha@gmail.com>
2023-06-25 14:19:36 -0500

    fixed bugs

--- src/test/java/com/example/addingNumbers/ControllerTests.java
+++ src/test/java/com/example/addingNumbers/ControllerTests.java
@@ -2,19 +2,20 @@ package com.example.addingNumbers;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class ControllerTests {
-
-@InjectMocks
+@ExtendWith(MockitoExtension.class)
+class ControllerTests {
+    @InjectMocks
     AddContoller contoller;
 
 @Test
     void addTwoNumbers(){
     int a=10;
     int b=10;
-    int r=a+b;
-    String res = "sum is "+ r;
+    String res=contoller.addTwoNumbers(a,b);
     Assertions.assertEquals("sum is 20",res);
 }
 }
